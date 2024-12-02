096ad3aee9541cb9a86dd1c9f0e1cedb90693ac5
MANTIZ <64778475+OMANTIZ@users.noreply.github.com>
2024-12-02 23:57:15 +0900

    置き換え

--- db/db.go
+++ db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,36 +10,24 @@ import (
 	"gorm.io/gorm"
 )
 
-// DBの起動
 func NewDB() *gorm.DB {
-
-	var flag_env = flag.String("GO_ENV", "", "開発環境フラグ")
-	flag.Parse()
-	// 環境変数のdevの場合は .envの内容を読み込む
-	//if os.Getenv("GO_ENV") == "dev" {
-	if *flag_env == "dev" {
+	if os.Getenv("GO_ENV") == "dev" {
 		err := godotenv.Load()
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
-
-	// URLを生成(.env内の情報をURLに埋め込む)
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
-
-	// postgreSQLへの接続
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("Connected")
+	fmt.Println("Connceted")
 	return db
-
 }
 
-// DBのクローズ
 func CloseDB(db *gorm.DB) {
 	sqlDB, _ := db.DB()
 	if err := sqlDB.Close(); err != nil {
